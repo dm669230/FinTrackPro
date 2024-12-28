6aa9c0f8cf97340a136b52b750bdd70fc9f1e195
Vaibhav <viabhav.shrivastava@factsonline.in>
2024-12-28 14:56:08 +0530

    code change

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends, HTTPException
+from fastapi import FastAPI,APIRouter, Depends, HTTPException, Request
 from functools import lru_cache
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
@@ -7,17 +7,16 @@ from app.routers.AdminRouter import router as admin_router
 from app.routers.LoanManageRouter import router as loan_manage_router
 from app.routers.RepayTrackingRouter import router as repay_router
 from app.config.db import SessionLocal, get_db
-from app.config import config
+from typing_extensions import Annotated
+from .config import config
 from dotenv import load_dotenv
-import uvicorn
 
 # Load environment variables from the .env file
 load_dotenv(override=True)
 
 app = FastAPI()
-
 origins = [
-    "http://localhost",
+    "http://localhost"
     "http://localhost:8080",
 ]
 
@@ -37,8 +36,10 @@ async def home():
 def get_settings():
     return config.Settings()
 
+print("my_settings",get_settings())
+
 @app.get("/info")
-async def info(settings: Depends(get_settings)):
+async def info(settings:Annotated[config.Settings, Depends(get_settings)]):
     return {
         "app_name": settings.app_name,
         "admin_email": settings.admin_email,
@@ -51,11 +52,11 @@ def health_check(db: Session = Depends(get_db)):
         db.execute("SELECT 1")  # Simple query to check DB connection
         return {"status": "healthy"}
     except Exception as e:
-        # Add logging for better debugging
         return {"status": "unhealthy", "error": str(e)}
 
 app.include_router(auth_router, prefix="/auth", tags=["AuthenticationAPI's"])
 app.include_router(admin_router, prefix="/admin", tags=["AdminAPI's"])
-app.include_router(loan_manage_router, prefix="/manageloan", tags=["LoanManageAPI's"])
-app.include_router(repay_router, prefix="/repayments", tags=["RepaymentsAPI's"])
+app.include_router(loan_manage_router, prefix="/manageloan",tags=["LoanManageAPI's"])
+app.include_router(repay_router, prefix="/repayments",tags=["RepaymentsAPI's"])
+
 
